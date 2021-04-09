4af6e08b66b97e0f09425a6226f6992dca28ee17
Gernika <novotel2006@ya.ru>
2021-04-09 19:14:24 +0100

    add  style, count dz2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, abort, request, redirect
+import random
+import string
+import os
 
 app = Flask(__name__)
 articles = [
@@ -19,17 +22,17 @@ articles = [
 
 ]
 
+users = [
+    {
+        'name': '',
+        'email': '',
+        'telephone': '',
+        'password': '',
+        'repit_password': '',
+        'avatar': ''
 
-# users = [
-#     {
-#         'name': '',
-#         'email': '',
-#         'telephone': '',
-#         'password': '',
-#         'repit_password': '',
-#         'avatar': ''
-#
-#     }]
+    }]
+
 
 @app.route('/')
 def main_page():
@@ -40,19 +43,20 @@ def main_page():
 def get_article(id):
     for article in articles:
         if article['id'] == id:
-            article['views_count']+=1
+            article['views_count'] += 1
             return render_template('generic.html', article=article, title='KsuZag')
     abort(404)
 
 
-@app.route('/create/article', methods=['GET', 'POST'])
+@app.route('/create/article', methods=['GET', 'POST'],)
 def create_article():
     '''GET,POST,PUT,DELETE'''
 
     if request.method == 'GET':
-        return render_template('create_article.html')
+        return render_template('create_article.html', title='KsuZag')
     elif request.method == 'POST':
         articles.append({
+            'id': len(articles) + 1,
             'views_count': 0,
             'author': request.form['art_author'],
             'title': request.form['art_title'],
@@ -63,9 +67,31 @@ def create_article():
         return 'METHOD BAD'
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html', title='KsuZag')
+@app.route('/update/article/<int:id>', methods=['GET', 'POST'])
+def update_article(id):
+    if request.method == 'GET':
+        for article in articles:
+            if article['id'] == id:
+                return render_template('update_article.html', article=article)
+            abort(404)
+    elif request.method == 'POST':
+        image = request.files['article_image']
+        for article in articles:
+            if article['id'] == id:
+                article['title'] = request.form['article_title']
+                article['author'] = request.form['article_author']
+                article['text'] = request.form['article_text']
+                if image.filename:
+                    random_name = ''.join([random.choice(string.digits + string.ascii_letters) for x in range(10)])
+                    img_path = f'static/images/{random_name}.jpg'
+                    image.save(img_path)
+                else:
+                    img_path = BASE_IMG_PATH
+                if article['img'] != BASE_IMG_PATH:
+                    os.remove(article['img'])
+
+                article['img'] = img_path
+                return redirect(f'/article/{article["id"]}')
 
 
 @app.route('/elements')
@@ -73,19 +99,24 @@ def elements():
     return render_template('elements.html', title='KsuZag')
 
 
-# @app.route('/create/user')
-# def create_user():
-#     if request.method == 'GET':
-#         return render_template('create_user.html')
-#     elif request.method == 'POST':
-#         for user in users:
-#             user['name'] = request.form['user_name']
-#             user['email'] = request.form['user_email']
-#             user['telephone'] = request.form['user_tel']
-#             user['password'] = request.form['user_password']
-#             user['repit_password'] = request.form['user_rep_pass']
-#             user['avatar'] = request.form['user_avatar']
-#             return redirect('/')
+@app.route('/about')
+def about():
+    return render_template('about.html', title='KsuZag')
+
+
+@app.route('/create/user')
+def create_user():
+    if request.method == 'GET':
+        return render_template('create_user.html')
+    elif request.method == 'POST':
+        for user in users:
+            user['name'] = request.form['user_name']
+            user['email'] = request.form['user_email']
+            user['telephone'] = request.form['user_tel']
+            user['password'] = request.form['user_password']
+            user['repit_password'] = request.form['user_rep_pass']
+            user['avatar'] = request.form['user_avatar']
+            return redirect('/')
 
 
 if __name__ == '__main__':
